5572628356f6ebac35a1f28da3b916600101e57c
blaudev <danielpeiro72@gmail.com>
2023-10-23 00:29:58 +0200

    Add ExpressionExtensions

---
+++ src/Blau/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Blau.Extensions;
+
+public static class ExpressionExtensions
+{
+    public static string GetFuncName<T>(this Expression<Func<T>> expr)
+    {
+        var name = expr.Body switch
+        {
+            MemberExpression memberExpression => memberExpression.Member.Name,
+            UnaryExpression unaryExpression => ((MemberExpression)unaryExpression.Operand).Member.Name,
+            _ => throw new ArgumentException("Expression is not a member access", nameof(expr))
+        };
+
+        return name;
+    }
+
+    public static T GeyFuncValue<T>(this Expression<Func<T>> expr)
+    {
+        return expr.Compile().Invoke();
+    }
+}
