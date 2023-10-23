6f3f71991c74741a704e14bc531cf31c6541f163
blaudev <danielpeiro72@gmail.com>
2023-10-23 17:33:37 +0200

    Add vehicle models facet and filter

---
+++ src/Blau/Exceptions/ObjectExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Blau.Exceptions;
+
+public static class ObjectExtensions
+{
+    public static bool HasValue<T>(this T obj) => obj != null;
+
+    public static T Value<T>(this T? obj) => obj ?? throw new NullReferenceException();
+}
