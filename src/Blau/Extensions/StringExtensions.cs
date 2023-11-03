96e2ef711491c5d11a691049e185fc2787fff905
blaudev <danielpeiro72@gmail.com>
2023-11-03 17:25:51 +0100

    Add ToSlug string extension

---
+++ src/Blau/Extensions/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace System;
+
+public static partial class StringExtensions
+{
+    public static string ToSlug(this string str, char separator = '=')
+    {
+        var separatorString = separator.ToString();
+
+        str = str.ToLower();
+
+        ARegex().Replace(str, "a");
+        ERegex().Replace(str, "e");
+        IRegex().Replace(str, "i");
+        ORegex().Replace(str, "o");
+        URegex().Replace(str, "u");
+
+        str = str.Replace("ñ", "n");
+        str = str.Replace("ç", "c");
+
+        SpacesRegex().Replace(str, separatorString);
+        str = str.Trim(separator);
+
+        return str;
+    }
+
+    [GeneratedRegex(@"[\s-_]+")]
+    private static partial Regex SpacesRegex();
+
+    [GeneratedRegex(@"[àá]")]
+    private static partial Regex ARegex();
+
+    [GeneratedRegex(@"[èé]")]
+    private static partial Regex ERegex();
+
+    [GeneratedRegex(@"[ìí]")]
+    private static partial Regex IRegex();
+
+    [GeneratedRegex(@"[òó]")]
+    private static partial Regex ORegex();
+
+    [GeneratedRegex(@"[ùú]")]
+    private static partial Regex URegex();
+}
